23e188390f42013f3716615f4573318dcd1e5f92
shash-786 <ssalian60@yahoo.com>
2023-09-02 19:41:17 +0530

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+    int arr[] = {3,2,1,0};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    std::sort(arr,arr+size);
+    for(auto i:arr){
+        std::cout << i << " ";
+    }
+    return 0;
+}
